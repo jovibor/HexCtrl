b1aa40e09dff07d4927f525e46751ee9f256f65c
datasynergyuk <48149668+datasynergyuk@users.noreply.github.com>
2020-03-27 16:23:59 +0000

    Extended data interpreter with binary, FILETIME, javatime, MS-DOS tiome, Windows SYSTEMTIME, GUID and GUID time

--- HexCtrl/src/Dialogs/CHexDlgDataInterpret.cpp
+++ HexCtrl/src/Dialogs/CHexDlgDataInterpret.cpp
@@ -15,6 +15,16 @@
 using namespace HEXCTRL;
 using namespace HEXCTRL::INTERNAL;
 
+//Time calculation constants
+//
+#define FTTICKSPERMS				10000			//Number of 100ns intervals in a milli-second
+#define FTTICKSPERSECOND			10000000		//Number of 100ns intervals in a second
+#define HOURSPERDAY					24				//24 hours per day
+#define SECONDSPERHOUR				3600			//3600 seconds per hour
+#define FILETIME1582OFFSETDAYS		6653			//FILETIME is based upon 1 Jan 1601 whilst GUID time is from 15 Oct 1582. Add 6653 days to convert to GUID time
+#define FILETIME1970_LOW			0xd53e8000		//1st Jan 1970 as FILETIME
+#define FILETIME1970_HIGH			0x019db1de		//Used for Unix and Java times
+
 BEGIN_MESSAGE_MAP(CHexDlgDataInterpret, CDialogEx)
 	ON_WM_CLOSE()
 	ON_WM_ACTIVATE()
@@ -46,6 +56,17 @@ BOOL CHexDlgDataInterpret::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
+	//Determine locale specific date format. Default to UK/European if unable to determine
+	//See: https://docs.microsoft.com/en-gb/windows/win32/intl/locale-idate
+	int nResult = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_IDATE | LOCALE_RETURN_NUMBER, (LPTSTR)&m_dwDateFormat, sizeof(m_dwDateFormat));
+	if (!nResult)
+		m_dwDateFormat = 1;
+
+	//Determine 'short' date seperator character. Default to UK/European if unable to determine	
+	nResult = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SDATE, m_wszDateSeparator, _countof(m_wszDateSeparator));
+	if (!nResult)
+		_stprintf_s(m_wszDateSeparator, _countof(m_wszDateSeparator), L"/");
+
 	if (auto pRadio = (CButton*)GetDlgItem(IDC_HEXCTRL_DATAINTERPRET_RADIO_LE); pRadio)
 		pRadio->SetCheck(1);
 	if (auto pRadio = (CButton*)GetDlgItem(IDC_HEXCTRL_DATAINTERPRET_RADIO_DEC); pRadio)
@@ -56,6 +77,7 @@ BOOL CHexDlgDataInterpret::OnInitDialog()
 	m_stCtrlGrid.EnableHeaderCtrl(TRUE, L"Data type", L"Value");
 	m_stCtrlGrid.GetHeaderCtrl().SetItem(0, &m_hdItemPropGrid); //Property grid column size.
 
+	m_vecProp.emplace_back(GRIDDATA { EGroup::DIGITS, EName::NAME_BINARY, ESize::SIZE_BYTE, new CMFCPropertyGridProperty(L"binary:", L"0") });
 	m_vecProp.emplace_back(GRIDDATA { EGroup::DIGITS, EName::NAME_CHAR, ESize::SIZE_BYTE, new CMFCPropertyGridProperty(L"char:", L"0") });
 	m_vecProp.emplace_back(GRIDDATA { EGroup::DIGITS, EName::NAME_UCHAR, ESize::SIZE_BYTE, new CMFCPropertyGridProperty(L"unsigned char:", L"0") });
 	m_vecProp.emplace_back(GRIDDATA { EGroup::DIGITS, EName::NAME_SHORT, ESize::SIZE_WORD, new CMFCPropertyGridProperty(L"short:", L"0") });
@@ -64,6 +86,7 @@ BOOL CHexDlgDataInterpret::OnInitDialog()
 	m_vecProp.emplace_back(GRIDDATA { EGroup::DIGITS, EName::NAME_ULONG, ESize::SIZE_DWORD, new CMFCPropertyGridProperty(L"unsigned long:", L"0") });
 	m_vecProp.emplace_back(GRIDDATA { EGroup::DIGITS, EName::NAME_LONGLONG, ESize::SIZE_QWORD, new CMFCPropertyGridProperty(L"long long:", L"0") });
 	m_vecProp.emplace_back(GRIDDATA { EGroup::DIGITS, EName::NAME_ULONGLONG, ESize::SIZE_QWORD, new CMFCPropertyGridProperty(L"unsigned long long:", L"0") });
+	m_vecProp.emplace_back(GRIDDATA{ EGroup::DIGITS, EName::NAME_GUID, ESize::SIZE_DQWORD, new CMFCPropertyGridProperty(L"GUID:", L"0") });
 	auto pDigits = new CMFCPropertyGridProperty(L"Digits:");
 	for (auto& iter : m_vecProp)
 		if (iter.eGroup == EGroup::DIGITS)
@@ -80,6 +103,11 @@ BOOL CHexDlgDataInterpret::OnInitDialog()
 
 	m_vecProp.emplace_back(GRIDDATA { EGroup::TIME, EName::NAME_TIME32T, ESize::SIZE_DWORD, new CMFCPropertyGridProperty(L"time32_t:", L"0") });
 	m_vecProp.emplace_back(GRIDDATA { EGroup::TIME, EName::NAME_TIME64T, ESize::SIZE_QWORD, new CMFCPropertyGridProperty(L"time64_t:", L"0") });
+	m_vecProp.emplace_back(GRIDDATA { EGroup::TIME, EName::NAME_FILETIME, ESize::SIZE_QWORD, new CMFCPropertyGridProperty(L"FILETIME:", L"0") });
+	m_vecProp.emplace_back(GRIDDATA{ EGroup::TIME, EName::NAME_JAVATIME, ESize::SIZE_QWORD, new CMFCPropertyGridProperty(L"Java time:", L"0") });
+	m_vecProp.emplace_back(GRIDDATA{ EGroup::TIME, EName::NAME_MSDOSTIME, ESize::SIZE_QWORD, new CMFCPropertyGridProperty(L"MS-DOS time:", L"0") });
+	m_vecProp.emplace_back(GRIDDATA{ EGroup::TIME, EName::NAME_SYSTEMTIME, ESize::SIZE_DQWORD, new CMFCPropertyGridProperty(L"Windows SYSTEMTIME:", L"0") });
+	m_vecProp.emplace_back(GRIDDATA{ EGroup::TIME, EName::NAME_GUIDTIME, ESize::SIZE_DQWORD, new CMFCPropertyGridProperty(L"GUID v1 UTC time:", L"0") });
 	auto pTime = new CMFCPropertyGridProperty(L"Time:");
 	for (auto& iter : m_vecProp)
 		if (iter.eGroup == EGroup::TIME)
@@ -320,14 +348,22 @@ void CHexDlgDataInterpret::InspectOffset(ULONGLONG ullOffset)
 	WCHAR buff[32];
 	std::wstring wstrFormat { };
 
+	//Binary
+	const WCHAR* nibbles[] = { L"0000", L"0001", L"0010", L"0011", L"0100", L"0101", L"0110", L"0111",	L"1000", L"1001", L"1010", L"1011", L"1100", L"1101", L"1110", L"1111" };
+	wstrFormat = L"%s%s";
+	
+	const auto byte = m_pHexCtrl->GetData<BYTE>(ullOffset);
+	swprintf_s(buff, _countof(buff), wstrFormat.data(), nibbles[byte >> 4], nibbles[byte & 0x0F]);
+	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
+		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_BINARY; }); iter != m_vecProp.end())
+		iter->pProp->SetValue(buff);
 
 	//Char
 	if (m_fShowAsHex)
 		wstrFormat = L"0x%hhX";
 	else
 		wstrFormat = L"%hhi";
-
-	const auto byte = m_pHexCtrl->GetData<BYTE>(ullOffset);
+	
 	swprintf_s(buff, _countof(buff), wstrFormat.data(), static_cast<char>(byte));
 	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
 		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_CHAR; }); iter != m_vecProp.end())
@@ -513,6 +549,141 @@ void CHexDlgDataInterpret::InspectOffset(ULONGLONG ullOffset)
 	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
 		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_TIME64T; }); iter != m_vecProp.end())
 		iter->pProp->SetValue(wstrTime.data());
+
+	//FILETIME	
+	wstrTime = L"N/A";
+	SYSTEMTIME SysTime = {0};
+	if (FileTimeToSystemTime(reinterpret_cast<const FILETIME*>(&qword), &SysTime))
+		wstrTime = SystemTimeToString(&SysTime, true, true).GetString();
+
+	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
+		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_FILETIME; }); iter != m_vecProp.end())
+		iter->pProp->SetValue(wstrTime.data());
+
+	//Javatime (signed)
+	wstrTime = L"N/A";
+	
+	FILETIME ftJavaTime;
+	if (qword >= 0)
+	{
+		LARGE_INTEGER Time;
+		Time.HighPart = FILETIME1970_LOW;
+		Time.LowPart = FILETIME1970_HIGH;
+		Time.QuadPart += qword * FTTICKSPERMS;
+		ftJavaTime.dwHighDateTime = Time.HighPart;
+		ftJavaTime.dwLowDateTime = Time.LowPart;		
+	}
+	else
+	{
+		LARGE_INTEGER Time;
+		Time.HighPart = FILETIME1970_LOW;
+		Time.LowPart = FILETIME1970_HIGH;
+		Time.QuadPart -= qword * FTTICKSPERMS;
+		ftJavaTime.dwHighDateTime = Time.HighPart;
+		ftJavaTime.dwLowDateTime = Time.LowPart;
+	}
+
+	if (FileTimeToSystemTime(&ftJavaTime, &SysTime))
+		wstrTime = SystemTimeToString(&SysTime, true, true).GetString();
+
+	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
+		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_JAVATIME; }); iter != m_vecProp.end())
+		iter->pProp->SetValue(wstrTime.data());
+
+	//MS-DOS date/time
+	typedef union _MSDOSDATETIME		//MS-DOS Date+Time structure (as used in FAT file system directory entry)
+	{									//See: https://msdn.microsoft.com/en-us/library/ms724274(v=vs.85).aspx
+		struct
+		{
+			WORD wTime;					//Time component
+			WORD wDate;					//Date component
+		} TimeDate;
+		DWORD dwTimeDate;
+	} MSDOSDATETIME, *PMSDOSDATETIME;
+	   	 
+	//Convert to FILETIME
+	wstrTime = L"N/A";
+	FILETIME ftMSDOS;
+	MSDOSDATETIME msdosDateTime;
+	dword = 0;
+	msdosDateTime.dwTimeDate = dword;	
+	if (DosDateTimeToFileTime(msdosDateTime.TimeDate.wDate, msdosDateTime.TimeDate.wTime, &ftMSDOS))
+	{
+		if (FileTimeToSystemTime(&ftMSDOS, &SysTime))
+			wstrTime = SystemTimeToString(&SysTime, true, true).GetString();
+	}
+
+	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
+		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_MSDOSTIME; }); iter != m_vecProp.end())
+		iter->pProp->SetValue(wstrTime.data());
+	
+	typedef union _DQWORD128
+	{									
+		struct
+		{
+			QWORD qwLow;
+			QWORD qwHigh;
+		} Value;
+		GUID gGUID;
+	} DQWORD128, *PDQWORD128;
+
+	auto dqword = m_pHexCtrl->GetData<DQWORD128>(ullOffset);	
+	if (m_fBigEndian)
+	{
+		//TODO: Test this thoroughly
+		QWORD tmp = dqword.Value.qwLow;
+		dqword.Value.qwLow = _byteswap_uint64(dqword.Value.qwHigh);
+		dqword.Value.qwHigh = _byteswap_uint64(tmp);
+	}
+
+	//Guid
+	CString sGUID;
+	sGUID.Format(_T("{%.8x-%.4x-%.4x-%.2x%.2x-%.2x%.2x%.2x%.2x%.2x%.2x}"), dqword.gGUID.Data1, dqword.gGUID.Data2, dqword.gGUID.Data3, dqword.gGUID.Data4[0], dqword.gGUID.Data4[1], dqword.gGUID.Data4[2], dqword.gGUID.Data4[3], dqword.gGUID.Data4[4], dqword.gGUID.Data4[5], dqword.gGUID.Data4[6], dqword.gGUID.Data4[7]);
+	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
+		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_GUID; }); iter != m_vecProp.end())
+		iter->pProp->SetValue(sGUID);
+
+	//Windows SYSTEMTIME
+	memcpy(&SysTime, &dqword, sizeof(SysTime));
+	wstrTime = SystemTimeToString(&SysTime, true, true).GetString();
+
+	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
+		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_SYSTEMTIME; }); iter != m_vecProp.end())
+		iter->pProp->SetValue(wstrTime.data());
+	
+	//Guid v1 Datetime UTC
+	//First, verify GUID is actually version 1 style
+	wstrTime = L"N/A";
+	unsigned short unGuidVersion = (dqword.gGUID.Data3 & 0xf000) >> 12;
+	if (unGuidVersion == 1)
+	{
+		LARGE_INTEGER qwGUIDTime;
+		qwGUIDTime.LowPart = dqword.gGUID.Data1;
+		qwGUIDTime.HighPart = dqword.gGUID.Data3 & 0x0fff;
+		qwGUIDTime.HighPart = (qwGUIDTime.HighPart << 16) | dqword.gGUID.Data2;
+
+		//RFC4122: The timestamp is a 60-bit value.  For UUID version 1, this is represented by Coordinated Universal Time (UTC) as a count of 100-
+		//nanosecond intervals since 00:00:00.00, 15 October 1582 (the date of Gregorian reform to the Christian calendar).
+		//
+		//Both FILETIME and GUID time are based upon 100ns intervals
+		//FILETIME is based upon 1 Jan 1601 whilst GUID time is from 1582. Subtract 6653 days to convert from GUID time
+		//NB: 6653 days from 15 Oct 1582 to 1 Jan 1601
+		//
+		ULARGE_INTEGER ullSubtractTicks;
+		ullSubtractTicks.QuadPart = QWORD(FTTICKSPERSECOND)*QWORD(SECONDSPERHOUR)*QWORD(HOURSPERDAY)*QWORD(FILETIME1582OFFSETDAYS);
+		qwGUIDTime.QuadPart -= ullSubtractTicks.QuadPart;
+
+		//Convert to SYSTEMTIME
+		FILETIME ftGUIDTime;
+		ftGUIDTime.dwHighDateTime = qwGUIDTime.HighPart;
+		ftGUIDTime.dwLowDateTime = qwGUIDTime.LowPart;
+		if (FileTimeToSystemTime(&ftGUIDTime, &SysTime))
+			wstrTime = SystemTimeToString(&SysTime, true, true).GetString();
+	}
+
+	if (auto iter = std::find_if(m_vecProp.begin(), m_vecProp.end(),
+		[](const GRIDDATA& refData) {return refData.eName == EName::NAME_GUIDTIME; }); iter != m_vecProp.end())
+		iter->pProp->SetValue(wstrTime.data());	
 }
 
 ULONGLONG CHexDlgDataInterpret::GetSize()
@@ -536,4 +707,46 @@ void CHexDlgDataInterpret::UpdateHexCtrl()
 {
 	if (m_pHexCtrl)
 		m_pHexCtrl->RedrawWindow();
+}
+
+CString CHexDlgDataInterpret::SystemTimeToString(PSYSTEMTIME pSysTime, bool bIncludeDate, bool bIncludeTime)
+{
+	if (pSysTime->wDay > 0 && pSysTime->wDay < 32 && pSysTime->wMonth>0 && pSysTime->wMonth < 13 && pSysTime->wYear < 10000 && pSysTime->wHour < 25 && pSysTime->wMinute < 61 && pSysTime->wSecond < 61 && pSysTime->wMilliseconds < 1000)
+	{
+		//Generate human formatted date. Fall back to UK/European if unable to determine
+		CString sResult;
+		if (bIncludeDate)
+		{
+			switch (m_dwDateFormat)
+			{
+				//0=Month-Day-Year
+			case 0:		sResult.Format(L"%.2d%s%.2d%s%.4d", pSysTime->wMonth, m_wszDateSeparator, pSysTime->wDay, m_wszDateSeparator, pSysTime->wYear);
+				break;
+
+				//2=Year-Month-Day 
+			case 2:		sResult.Format(L"%.4d%s%.2d%s%.2d", pSysTime->wYear, m_wszDateSeparator, pSysTime->wMonth, m_wszDateSeparator, pSysTime->wDay);
+				break;
+
+				//1=Day-Month-Year (default)
+			default:	sResult.Format(L"%.2d%s%.2d%s%.4d", pSysTime->wDay, m_wszDateSeparator, pSysTime->wMonth, m_wszDateSeparator, pSysTime->wYear);
+				break;
+			}
+
+			if (bIncludeTime)
+				sResult.Append(L" ");
+		}
+
+		//Append optional time elements
+		if (bIncludeTime)
+		{
+			sResult.AppendFormat(L"%.2d:%.2d:%.2d", pSysTime->wHour, pSysTime->wMinute, pSysTime->wSecond);
+
+			if (pSysTime->wMilliseconds > 0)
+				sResult.AppendFormat(L".%.3d", pSysTime->wMilliseconds);
+		}
+
+		return sResult;
+	}
+	else
+		return L"N/A";
 }
