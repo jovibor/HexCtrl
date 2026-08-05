6cf5379df8a119858d1f245a47edb2cbc9b412f5
datasynergyuk <48149668+datasynergyuk@users.noreply.github.com>
2026-08-05 11:20:03 +1000

    New notification message HEXCTRL_MSG_SETOFFSETMODE added, when hex/decimal display mode changes. (#100)

--- HexCtrl/HexCtrl.h
+++ HexCtrl/HexCtrl.h
@@ -526,8 +526,9 @@ namespace HEXCTRL {
 	constexpr auto HEXCTRL_MSG_SETDATA { 0x010EU };       //Indicates that the data has changed.
 	constexpr auto HEXCTRL_MSG_SETFONT { 0x010FU };       //Font has changed.
 	constexpr auto HEXCTRL_MSG_SETGROUPSIZE { 0x0110U };  //Data grouping size has changed.
-	constexpr auto HEXCTRL_MSG_SETSELECTION { 0x0111U };  //Selection has been made.
-
+	constexpr auto HEXCTRL_MSG_SETOFFSETMODE { 0x0111U }; //Offset display mode (Hex/Decimal) has changed.
+	constexpr auto HEXCTRL_MSG_SETSELECTION { 0x0112U };  //Selection has been made.
+	
 	/**************************************************************************
 	* Flags for the internal dialogs, used with the SetDlgProperties method.  *
 	**************************************************************************/
